878c756f95f627fd91529e7db8c8c6d1c0ccdbed
Jack Benarroch Jedlicki <124214463+jackjedlicki@users.noreply.github.com>
2023-04-23 19:48:17 -0700

    Update create_dataset_7pars.py

--- create_dataset_7pars.py
+++ create_dataset_7pars.py
@@ -39,7 +39,7 @@ def Elastance(Emax,Emin,t, Tc):
     return (Emax-Emin)*1.55*(tn/0.7)**1.9/((tn/0.7)**1.9+1)*1/((tn/1.17)**21.9+1) + Emin
      
 # Define your function here (for example, a 2-variable function)
-def f(Tc, start_v, startp, Rc, Emax, Emin, Vd):
+def f(Tc, start_v, startp, Rc, Emax, Emin, Vd, Ca):
 
     N = 10
     start_pla = float(start_v*Elastance(Emax, Emin, 0, Tc))
@@ -56,7 +56,6 @@ def f(Tc, start_v, startp, Rc, Emax, Emin, Vd):
     Rm=float(0.0050)
     Ra=float(0.0010)
     Rc=float(0.06)
-    Ca=float(0.0800)
     Cs=float(1.3300)
     Cr=float(4.400)
     Ls=float(0.0005)
@@ -106,39 +105,160 @@ for Vd in vds:
         for Rc in rcs:
           for Emax in emaxs:
             for Emin in emins:
-              vedest, vesest, minv, minp = f(Tc, start_v, startp, Rc, Emax, Emin, Vd)
-              vedssim.append(vedest)
-              vesssim.append(vesest)
-              pts.append([Tc, start_v, startp, Rc, Emax, Emin, Vd])
-              if (minv<=0 or minp<=0): 
-                errors.append(i)
-                print("Error", i)
-              if (i%1000==0): print(i)
-              i+=1
+              for Ca in cas:
+                vedest, vesest, minv, minp = f(Tc, start_v, startp, Rc, Emax, Emin, Vd, Ca)
+                vedssim.append(vedest)
+                vesssim.append(vesest)
+                pts.append([Tc, start_v, startp, Rc, Emax, Emin, Vd, Ca])
+                if (minv<=0 or minp<=0): 
+                  errors.append(i)
+                  print("Error", i)
+                if (i%1000==0): print(i)
+                i+=1
 
 print("done")
-              
-output_path = '/content/drive/My Drive/'
+print("errors:", errors)
 
-file = 'points_8pars'
+#convert into torch tensors:
 pts2 = torch.zeros(N,npars)
 for i in range(N):
   for j in range(npars):
     pts2[i][j] = pts[i][j]
-torch.save(pts2, os.path.join(output_path,f'{file}.pt'))
 
-file = 'veds_8pars'
-vedssim2 = torch.zeros(N)
-for i in range(NotADirectoryError):
-    vedssim2[i] = vedssim[i]
-torch.save(vedssim2, os.path.join(output_path,f'{file}.pt'))
-
-file = 'vess_8pars'
-vesssim2 = torch.zeros(N)
+veds2 = torch.zeros(N,2)
 for i in range(N):
-    vesssim2[i] = vesssim[i]
-torch.save(vesssim, os.path.join(output_path,f'{file}.pt'))
+    veds2[i][0] = vedssim[i]
+    veds2[i][1] = vesssim[i]
 
+#save it:
+from google.colab import drive
+drive.mount('/content/drive')  
+output_path = '/content/drive/My Drive/'
+file = 'points_8pars'
+torch.save(pts2, os.path.join(output_path,f'{file}.pt'))
+file = 'veds_8pars'
+torch.save(veds2, os.path.join(output_path,f'{file}.pt'))
 print("saved")
 
-print("errors:", errors)
+#create and train the interpolator:
+# Define the input and output tensors
+x = torch.tensor(pts2, dtype = torch.float64) # 7-dimensional input tensor
+# x = x.view(64, 3)
+y = torch.tensor(veds2, dtype=torch.float64) # 3-dimensional output tensor
+
+#use it for training the NN:
+
+class Interpolator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(8, 96).double()
+        self.fc2 = nn.Linear(96, 2).double()
+
+    def forward(self, z):
+        z = torch.relu(self.fc1(z))
+        z = self.fc2(z)
+        return z
+
+# Initialize the neural network
+net = Interpolator()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+losses = []
+d1 = 0
+d2 = 0
+
+# Train the neural network
+for epoch in range(10000):
+    # Forward pass
+    y_pred = net(x)
+    loss = criterion(y_pred, y)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Print progress
+    if epoch % 5000 == 0:
+        print(f'Epoch {epoch}, loss: {loss.item():.4f}')
+        losses.append(loss.item())
+        
+    if (loss.item()<20. and d1==0):
+      d1 = 1
+      optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+      
+    if (loss.item()<4. and d2==0):
+      d2 = 1
+      optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+
+print("training error", loss.item())
+
+#for testing the interpolator:
+N_test = 3
+
+x_test_tc = torch.zeros(N_test, npars)
+y_test_tc = torch.zeros(N_test, 2)
+
+for i in range(N_test):
+  x_test_tc[i][0] = random.uniform(0.5, 2.)
+  x_test_tc[i][1] = random.uniform(-20., 400.)
+  x_test_tc[i][2] = random.uniform(40., 110.)
+  x_test_tc[i][3] = random.uniform(0.02, 0.09)
+  x_test_tc[i][4] = random.uniform(0.2, 16.)
+  x_test_tc[i][5] = random.uniform(0.02, 0.1)
+  x_test_tc[i][6] = random.uniform(4., 15.)
+  x_test_tc[i][7] = random.uniform(0.05, 0.11)
+
+  #ved, ves, vmin, pmin = f(x_test_tc[i][0], x_test_tc[i][1], x_test_tc[i][2], x_test_tc[i][3], x_test_tc[i][4], x_test_tc[i][5], x_test_tc[i][6], x_test_tc[i][7])
+  ved, ves, vmin, pmin = f(x_test_tc[i][0].item(), x_test_tc[i][1].item(), x_test_tc[i][2].item(), x_test_tc[i][3].item(), x_test_tc[i][4].item(), x_test_tc[i][5].item(), x_test_tc[i][6].item(), x_test_tc[i][7].item())
+  y_test_tc[i][0] = ved
+  y_test_tc[i][1] = ves
+
+error = 0
+
+xt = torch.tensor(x_test_tc, dtype = torch.float64) # 7-dimensional input tensor
+# x = x.view(64, 3)
+yt = torch.tensor(y_test_tc, dtype=torch.float64) # 3-dimensional output tensor
+
+simveds=[]
+simvess=[]
+simefs=[]
+
+for i in range(N_test):
+  y_pred = net(xt[i])
+  vedsim = y_pred[0].detach().item()
+  vessim = y_pred[1].detach().item()
+  print(vedsim, "real", yt[i][0].item())
+  error += abs(vedsim - yt[i][0]) + abs(vessim - yt[i][1])
+  simveds.append(vedsim)
+  simvess.append(vessim)
+  simefs.append((vedsim-vessim)/vedsim*100.)
+
+print("Test error:", error / (N_test*2))
+
+#save the weights of net if it works well enough:
+if (error/(N_test*2)<1.):
+  import os
+  output_path = '/content/drive/My Drive/'
+  file = 'model_net_vedves_6000'
+  torch.save(net.state_dict(), os.path.join(output_path,f'{file}__weight.pt'))
+
+#plot the results: (real vs estimated ved, ves, ef)
+iters=np.linspace(1,N_test,N_test)
+efs=[]
+veds=[]
+vess=[]
+for i in range(N_test):
+  veds.append(y_test_tc[i][0])
+  vess.append(y_test_tc[i][1])
+  efs.append((y_test_tc[i][0]-y_test_tc[i][1])/y_test_tc[i][0]*100)
+
+plt.plot(iters,simveds, color='r')
+plt.plot(iters, veds, color='b')
+plt.show()
+plt.plot(iters,simvess, color='r')
+plt.plot(iters, vess, color='b')
+plt.show()
+plt.plot(iters,simefs, color='r')
+plt.plot(iters, efs, color='b')
+plt.show()
